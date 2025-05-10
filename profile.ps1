c3de7636896549861b1786940d9a3d6393bbb099
Erik <erik@erikhauser.com>
2025-05-10 12:32:08 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,24 @@
+$debug = $false
+
+# Define the path to the file that stores the last execution time
+$timeFilePath = [Environment]::GetFolderPath("MyDocuments") + "\PowerShell\LastExecutionTime.txt"
+
+# Define the update interval in days, set to -1 to always check
+$updateInterval = -1
+
+if ($debug) {
+    Write-Host "#######################################" -ForegroundColor Red
+    Write-Host "#           Debug mode enabled        #" -ForegroundColor Red
+    Write-Host "#          ONLY FOR DEVELOPMENT       #" -ForegroundColor Red
+    Write-Host "#                                     #" -ForegroundColor Red
+    Write-Host "#       IF YOU ARE NOT DEVELOPING     #" -ForegroundColor Red
+    Write-Host "#      JUST RUN \`Update-Profile\`    #" -ForegroundColor Red
+    Write-Host "#        to discard all changes       #" -ForegroundColor Red
+    Write-Host "#   and update to the latest profile  #" -ForegroundColor Red
+    Write-Host "#               version               #" -ForegroundColor Red
+    Write-Host "#######################################" -ForegroundColor Red
+}
+
 #################################################################################################################################
 ############                                                                                                         ############
 ############                                          !!!   WARNING:   !!!                                           ############
@@ -13,8 +34,18 @@
 ############                                                                                                         ############
 #################################################################################################################################
 
+# Initial GitHub.com connectivity check with 1 second timeout
+$global:canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
+
+# Import Modules and External Profiles
+# Ensure Terminal-Icons module is installed before importing
+if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+}
+Import-Module -Name Terminal-Icons
+
 # Check for Personal Profile Updates
-<# function Update-Personal {
+function Update-Personal {
     try {
         $url = "https://raw.githubusercontent.com/ehause0613/personalpwsh/main/profile.ps1"
         $oldhash = Get-FileHash $HOME/Documents/PowerShell/profile.ps1
@@ -34,12 +65,29 @@
     finally {
         Remove-Item "$env:temp/profile.ps1" -ErrorAction SilentlyContinue
     }
-} #>
+}
+
+# Check if not in debug mode AND (updateInterval is -1 OR file doesn't exist OR time difference is greater than the update interval)
+if (-not $debug -and `
+    ($updateInterval -eq -1 -or `
+            -not (Test-Path $timeFilePath) -or `
+        ((Get-Date) - [datetime]::ParseExact((Get-Content -Path $timeFilePath), 'yyyy-MM-dd', $null)).TotalDays -gt $updateInterval)) {
+
+    Update-Profile
+    $currentTime = Get-Date -Format 'yyyy-MM-dd'
+    $currentTime | Out-File -FilePath $timeFilePath
+
+}
+elseif ($debug) {
+    Write-Warning "Skipping profile update check in debug mode"
+}
 
 # Weather
-function Wx { (Invoke-WebRequest https://wttr.in).Content }
+function Get-Wx { (Invoke-WebRequest https://wttr.in).Content }
 
 # Network Utilities
-function PubIP { (Invoke-WebRequest https://ifconfig.me/ip).Content }
+function Get-PubIP { (Invoke-WebRequest https://ifconfig.me/ip).Content }
 
 function NetIP {Get-NetIPConfiguration}
+
+function WG { winget upgrade --all --accept-package-agreements --accept-source-agreements --silent --force --include-unknown}
