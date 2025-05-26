20b937ed470b031691f41baa84d80675abefdf4f
Erik <erik@erikhauser.com>
2025-05-25 23:20:05 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -96,6 +96,8 @@ function IPInfo {
     }
 }
 
+function DNS {Get-Dns}
+
 # WinGet App Updates
 function WG { winget upgrade --all --accept-package-agreements --accept-source-agreements --silent --force --include-unknown}
 
