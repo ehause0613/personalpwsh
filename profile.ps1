faf071be70ab1d5e6ed36762b3b70f8cc07ec728
Erik <erik@erikhauser.com>
2026-05-19 14:12:16 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -95,7 +95,7 @@ function Wx { (Invoke-WebRequest https://wttr.in).Content }
 ############                                                                                                         ############
 #################################################################################################################################
 
-function Time { (Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/coordinate") }
+function Time { (Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/America_New_York",  Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/Europe_Dublin")}
 
 #################################################################################################################################
 ############                                                                                                         ############
