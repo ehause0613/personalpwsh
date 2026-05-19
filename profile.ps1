6083966f65368d0189d58374d7f1e1815d9703e8
Erik <erik@erikhauser.com>
2026-05-19 14:30:17 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -97,8 +97,8 @@ function Wx { (Invoke-WebRequest https://wttr.in).Content }
 
 
 function Time {
-    Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/America_New_York"
-    Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/Europe_Dublin"
+    Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/zone?timeZone=America/New_York"
+    Invoke-RestMethod -Uri "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Dublin"
 }
 
 
